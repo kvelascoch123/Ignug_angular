56321d44d88c5ca720f5774adf69fed69a818e1b
kvelascoch123 <kvelascosr@gmail.com>
2019-06-09 21:37:18 -0500

    Tercer commit add two Interface

---
+++ src/app/interfaces/estudiante.ts
@@ -0,0 +1,5 @@
+export interface Estudiante {
+    id: number;
+    codigo: string;
+    correoInstitucional: string;
+}
---
+++ src/app/interfaces/persona.ts
@@ -0,0 +1,11 @@
+// tslint:disable-next-line:no-empty-interface
+export interface Persona {
+    nombre: string;
+    apellido: string;
+    edad: number;
+    cedula: number;
+    //  carrera
+    // malla
+    // materia
+    //
+}
